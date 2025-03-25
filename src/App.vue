09f3dc0237be05ee3110460db7bb605c2b2be1df
ErikCuevas03 <ryuekq@gmail.com>
2025-03-24 18:37:29 -0600

    task #0.1 subiendo proyecto

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import card from './components/card.vue';
+</script>
+
+<template>
+ <div>
+    <h1 style="text-align: center;">Mascota</h1>
+  <card/>
+
+ </div>
+</template>
+
+<style scoped>
+
+</style>
